79f419e694ab5e41fefa2bc5bd5a78bf2540e275
Azuna <36605286+azunaVT@users.noreply.github.com>
2025-06-08 19:48:35 +0000

    Improved tests by adding a nix flake version check

--- test/nix-dood/test.sh
+++ test/nix-dood/test.sh
@@ -4,7 +4,8 @@ source test-utils.sh
 
 # Template specific tests
 check "distro" lsb_release -c
-check "greeting" [ $(cat /home/code/greeting.txt | grep dev) ]
+check "greeting" [ $(cat /home/code/greeting.txt | grep "Welcome to your declarative, Nix-powered, dev environment setup.") ]
+check "nix develop" [ $(nix flake --version | grep 'nix (Nix)') ]
 
 # Report result
 reportResults
